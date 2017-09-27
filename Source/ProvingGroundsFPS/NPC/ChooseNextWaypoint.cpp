9b26ac71f3ebad285cab5452c9ddc6eb5ed7aa16
FIVE-PC\five <0dd05yy@gmail.com>
2017-09-27 18:00:52 -0500

    237 -- stable commit

---
+++ Source/ProvingGroundsFPS/NPC/ChooseNextWaypoint.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ChooseNextWaypoint.h"
+#include "AIController.h"
+#include "NPC/PatrolRoute.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
+{	
+	//GetPatrolPoints
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
+
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrolPoints.Num() == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Patrol Points not found."));
+		return EBTNodeResult::Failed;
+	}
+	
+	//Set next waypoint
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	//cycle the index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+	return EBTNodeResult::Succeeded;
+}
+
+
+
